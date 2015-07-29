aa6693351c4391def8bf95202f9f375925ffbae7
Shu Uesugi <shu@chibicode.com>
2015-07-29 05:59:21 -0700

    Experiment: Remove JS

--- _assets/app.js
+++ _assets/app.js
@@ -1,13 +1,13 @@
 require('./css/app.css');
-require('./js/transition.js');
-require('./js/zoom.js');
-require('./js/jquery.fitvids.js');
+// require('./js/transition.js');
+// require('./js/zoom.js');
+// require('./js/jquery.fitvids.js');
 
-import $ from 'jquery';
-import FastClick from 'fastclick';
+// import $ from 'jquery';
+// import FastClick from 'fastclick';
 
-$(function() {
-  FastClick.attach(document.body);
-  $('a[href^="http"]').attr('target','_blank');
-  $(".js-post-content").fitVids();
-});
+// $(function() {
+//   FastClick.attach(document.body);
+//   $('a[href^="http"]').attr('target','_blank');
+//   $(".js-post-content").fitVids();
+// });
